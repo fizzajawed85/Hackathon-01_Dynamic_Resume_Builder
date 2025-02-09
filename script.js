d24a368ec4da73b58e51ba571abb086b95279b36
fizzajawed85 <angelfizza85@gmail.com>
2025-02-09 14:00:39 +0500

    Updated project

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-/* Access references for the input and display zones */
-var form = document.getElementById("resume-form");
-var resumeDisplayElement = document.getElementById("resume-display");
-/* Handle Form Submission */
-form.addEventListener("submit", function (event) {
-    event.preventDefault(); // prevent page reload
-    // Collect input values
-=======
 // Access references for the input and display zones
 var form = document.getElementById("resume-form");
 var resumeDisplayElement = document.getElementById("resume-display");
@@ -16,7 +7,6 @@ form.addEventListener("submit", function (event) {
     event.preventDefault(); // prevent page reload
     // Collect input values
     var profilePictureInput = document.getElementById("profile-picture");
->>>>>>> 958d4c5 (Updated resume for better responsiveness)
     var firstName = document.getElementById("first-name").value;
     var lastName = document.getElementById("last-name").value;
     var emailAdress = document.getElementById("email-address").value;
@@ -24,16 +14,11 @@ form.addEventListener("submit", function (event) {
     var education = document.getElementById("education").value;
     var experience = document.getElementById("experience").value;
     var skills = document.getElementById("skills").value;
-<<<<<<< HEAD
-    // Generate resume content dynamically
-    var resumeHTML = "\n       <h2><b>Resume</b></h2>\n       <h3>Personal Information</h3>\n       <p><b>FirstName:</b>".concat(firstName, "</p>\n       <p><b>LastName:</b>").concat(lastName, "</p>\n       <p><b>Email:</b>").concat(emailAdress, "</p>\n       <p><b>Phone:</b>").concat(phoneNumber, "</p>\n\n       <h3>Education</h3>\n       <p>").concat(education, "</p>\n\n       <h3>Experience</h3>\n       <p>").concat(experience, "</p>\n\n       <h3>Skills</h3>\n       <p>").concat(skills, "</p>\n      \n     ");
-=======
     // Picture Elements
     var profilePictureFile = (_a = profilePictureInput.files) === null || _a === void 0 ? void 0 : _a[0];
     var profilePictureURL = profilePictureFile ? URL.createObjectURL(profilePictureFile) : '';
     // Generate resume content dynamically
     var resumeHTML = "\n    <h2><b>Resume</b></h2>\n    ".concat(profilePictureURL ? "<img src=\"".concat(profilePictureURL, "\" alt=\"Profile Picture\" class=\"profilePicture\">") : "", "\n    <h3>Personal Information</h3>\n    <p><b>First Name:</b> ").concat(firstName, "</p>\n    <p><b>Last Name:</b> ").concat(lastName, "</p>\n    <p><b>Email:</b> ").concat(emailAdress, "</p>\n    <p><b>Phone:</b> ").concat(phoneNumber, "</p>\n\n    <h3>Education</h3>\n    <p>").concat(education, "</p>\n\n    <h3>Experience</h3>\n    <p>").concat(experience, "</p>\n\n    <h3>Skills</h3>\n    <p>").concat(skills, "</p>\n  ");
->>>>>>> 958d4c5 (Updated resume for better responsiveness)
     // Display generated resume
     if (resumeDisplayElement) {
         resumeDisplayElement.innerHTML = resumeHTML;
--- script.ts
+++ script.ts
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-/* Access references for the input and display zones */
-
-const form = document.getElementById("resume-form") as HTMLFormElement;
-const resumeDisplayElement = document.getElementById("resume-display") as HTMLDivElement;
-
-/* Handle Form Submission */
-
-form.addEventListener("submit" , (event: Event) =>{
-     event.preventDefault(); // prevent page reload
-
-     // Collect input values
-     const firstName = (document.getElementById("first-name") as HTMLInputElement).value
-     const lastName = (document.getElementById("last-name") as HTMLInputElement).value
-     const emailAdress = (document.getElementById("email-address") as HTMLInputElement).value
-     const phoneNumber = (document.getElementById("phone-number") as HTMLInputElement).value
-     const education = (document.getElementById("education") as HTMLInputElement).value
-     const experience = (document.getElementById("experience") as HTMLInputElement).value
-     const skills = (document.getElementById("skills") as HTMLInputElement).value   
-     
-     // Generate resume content dynamically
-
-     const resumeHTML = `
-       <h2><b>Resume</b></h2>
-       <h3>Personal Information</h3>
-       <p><b>FirstName:</b>${firstName}</p>
-       <p><b>LastName:</b>${lastName}</p>
-       <p><b>Email:</b>${emailAdress}</p>
-       <p><b>Phone:</b>${phoneNumber}</p>
-
-       <h3>Education</h3>
-       <p>${education}</p>
-
-       <h3>Experience</h3>
-       <p>${experience}</p>
-
-       <h3>Skills</h3>
-       <p>${skills}</p>
-      
-     `;
-
-     // Display generated resume
-
-     if (resumeDisplayElement) {
-        resumeDisplayElement.innerHTML = resumeHTML;
-     } else {
-        console.error("The resume display is invalid.");
-        
-     }
-})
-=======
 // Access references for the input and display zones
 const form = document.getElementById("resume-form") as HTMLFormElement;
 const resumeDisplayElement = document.getElementById("resume-display") as HTMLDivElement;
@@ -98,4 +47,4 @@ form.addEventListener("submit", (event: Event) => {
     console.error("The resume display is invalid.");
   }
 });
->>>>>>> 958d4c5 (Updated resume for better responsiveness)
+
